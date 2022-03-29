f419133ddf04863747df8fd3ea53da56f873a5bc
Victor <vitya898989@gmail.com>
2022-03-29 03:12:10 +0300

    карта 1.1

--- sanis/js/script.js
+++ sanis/js/script.js
@@ -206,18 +206,48 @@ new Swiper('.novelties_slider', {
         }
     }
 });;
-ymaps.ready(init);
+
+let mainMapShops = document.querySelector('.main_map__shops');
+let mainMapOffices = document.querySelector('.main_map__offices');
+
+mainMapShops.onclick = () => {
+    mainMapShops.classList.add('active');
+    mainMapOffices.classList.remove('active');
+}
+mainMapOffices.onclick = () => {
+    mainMapOffices.classList.add('active');
+    mainMapShops.classList.remove('active');
+}
+
+let scriptMap = document.createElement('script');
+scriptMap.src = 'https://api-maps.yandex.ru/2.1/?apikey=dd0f5e76-e8bb-42be-b558-f7af3b491cd2&lang=ru_RU';
+setTimeout(() => document.head.append(scriptMap), 0);
+scriptMap.onload = function () {
+    ymaps.ready(init);
+};
 
 function init() {
-    var myMap = new ymaps.Map("map", {
-        center: [55.752648, 37.603846],
-        zoom: 17
+    var myMap = new ymaps.Map("main_map", {
+        center: [59.972205, 30.410512],
+        zoom: 12
     });
-    var placemark = new ymaps.Placemark([55.752648, 37.603846], {
-            iconColor: 'blue'
-        }
-    );
-    myMap.geoObjects.add(placemark);
+    var coords = [
+        [60.001523, 30.299438],
+        [59.944440, 30.491597],
+        [59.935818, 30.325300]
+    ];
+    var myCollection = new ymaps.GeoObjectCollection({}, {
+        iconLayout: 'default#image',
+        iconImageHref: '/img/main/map_point.svg',
+        iconImageSize: [31, 40],
+        iconImageOffset: [-13, -42]
+    });
+
+    for (var i = 0; i < coords.length; i++) {
+        var myPlacemark = new ymaps.Placemark(coords[i], {}, {});
+        myCollection.add(myPlacemark);
+    }
+    myMap.geoObjects.add(myCollection);
 };
 let footerCopyright = document.querySelectorAll('.footer__copyright');
 let footerSocial = document.querySelectorAll('.footer__social');
