a77771a9500e4d8ecf834ca23ce3895435ef077c
Victor <vitya898989@gmail.com>
2022-03-30 09:12:44 +0300

    fix слайдер 2

--- sanis/js/script.js
+++ sanis/js/script.js
@@ -74,22 +74,24 @@ window.addEventListener('resize', () => {
         document.querySelector('.header__bottom_content').insertBefore(headerCounts, document.querySelector('.header__icons_search'));
     }
 });;
-let mainSlider = new Swiper('.main_slider', {
-    speed: 1000,
-    slidesPerView: 1,
-    autoHeight: true,
-    navigation: {
-        nextEl: '.main_slider__btn-next',
-        prevEl: '.main_slider__btn-prev'
-    },
-    pagination: {
-        el: '.main_slider__pagination',
-        clickable: true,
-    },
-    autoplay: {
-        delay: 6850,
-    },
-});
+setTimeout(() => {
+    let mainSlider = new Swiper('.main_slider', {
+        speed: 1000,
+        slidesPerView: 1,
+        autoHeight: true,
+        navigation: {
+            nextEl: '.main_slider__btn-next',
+            prevEl: '.main_slider__btn-prev'
+        },
+        pagination: {
+            el: '.main_slider__pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 6850,
+        },
+    });
+}, 200);
 setTimeout(() => { mainSlider.updateAutoHeight() }, 350);;
 // Кнопка "Показать всех"
 
--- src/components/main_slider/main_slider.js
+++ src/components/main_slider/main_slider.js
@@ -1,17 +1,19 @@
-let mainSlider = new Swiper('.main_slider', {
-    speed: 1000,
-    slidesPerView: 1,
-    autoHeight: true,
-    navigation: {
-        nextEl: '.main_slider__btn-next',
-        prevEl: '.main_slider__btn-prev'
-    },
-    pagination: {
-        el: '.main_slider__pagination',
-        clickable: true,
-    },
-    autoplay: {
-        delay: 6850,
-    },
-});
+setTimeout(() => {
+    let mainSlider = new Swiper('.main_slider', {
+        speed: 1000,
+        slidesPerView: 1,
+        autoHeight: true,
+        navigation: {
+            nextEl: '.main_slider__btn-next',
+            prevEl: '.main_slider__btn-prev'
+        },
+        pagination: {
+            el: '.main_slider__pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 6850,
+        },
+    });
+}, 200);
 setTimeout(() => { mainSlider.updateAutoHeight() }, 350);
