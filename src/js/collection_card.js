d6627202d38c5c73998c30b8c137535905f72b1d
Victor <vitya898989@gmail.com>
2022-04-12 21:39:13 +0300

    две страницы

---
+++ src/js/collection_card.js
@@ -0,0 +1,4 @@
+@@include('../components/preloader/preloader.js');
+@@include('../components/header/header.js');
+@@include('../components/footer/footer.js');
+@@include('./item_cards.js');
---
+++ src/js/search_result.js
@@ -0,0 +1,6 @@
+@@include('../components/preloader/preloader.js');
+@@include('../components/header/header.js');
+@@include('../components/footer/footer.js');
+@@include('../components/catalog/catalog_filter/catalog_filter.js');
+@@include('../components/catalog/catalog_body/catalog_body.js');
+@@include('./item_cards.js');
