5870a14bc836aa4b0c7a62e9ad727f88894208ad
Victor <vitya898989@gmail.com>
2022-04-26 11:43:50 +0300

    страницы с формами

--- sanis/js/ordering.js
+++ sanis/js/ordering.js
@@ -134,26 +134,26 @@ let footerSocial = document.querySelectorAll('.footer__social');
 let footerContent = document.querySelectorAll('.footer__content');
 let footerMenuSocial = document.querySelectorAll('.footer__menu_social');
 
-if (window.innerWidth < 959) {
-    for (let i = 0; i < footerContent.length; i++) {
-        footerContent[i].append(footerCopyright[i]);
-        footerContent[i].append(footerSocial[i]);
-    }
-}
-
-window.addEventListener('resize', () => {
-    if (window.innerWidth < 959) {
-        for (let i = 0; i < footerContent.length; i++) {
-            footerContent[i].append(footerCopyright[i]);
-            footerContent[i].append(footerSocial[i]);
-        }
-    } else {
-        for (let i = 0; i < footerContent.length; i++) {
-            footerMenuSocial[i].append(footerCopyright[i]);
-            footerMenuSocial[i].append(footerSocial[i]);
-        }
-    }
-});;
+// if (window.innerWidth < 959) {
+//     for (let i = 0; i < footerContent.length; i++) {
+//         footerContent[i].append(footerCopyright[i]);
+//         footerContent[i].append(footerSocial[i]);
+//     }
+// }
+//
+// window.addEventListener('resize', () => {
+//     if (window.innerWidth < 959) {
+//         for (let i = 0; i < footerContent.length; i++) {
+//             footerContent[i].append(footerCopyright[i]);
+//             footerContent[i].append(footerSocial[i]);
+//         }
+//     } else {
+//         for (let i = 0; i < footerContent.length; i++) {
+//             footerMenuSocial[i].append(footerCopyright[i]);
+//             footerMenuSocial[i].append(footerSocial[i]);
+//         }
+//     }
+// });;
 
 let validateInputs = document.querySelectorAll('.js_validate');
 let registrationForm = document.querySelector('.ordering_form');
